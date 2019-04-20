5875c74795a5b83c209794f727e654b56e0c1562
Hosein Hadipour <hsn.hadipour@gmail.com>
2019-04-20 18:05:46 -0400

    indentation converted to spaces

--- polygen.h
+++ polygen.h
@@ -8,20 +8,20 @@
 
 #define EQ1 "L_%d_%d + %d"
 #define EQ2 "L_%d_%d + k_%d_%d + L_%d_%d + L_%d_%d + L_%d_%d + L_%d_%d"                \
-			" + L_%d_%d*L_%d_%d + b_%d_%d + L_%d_%d*L_%d_%d + L_%d_%d*L_%d_%d"         \
-			" + L_%d_%d*L_%d_%d + L_%d_%d*L_%d_%d + b_%d_%d*L_%d_%d + b_%d_%d*L_%d_%d" \
-			" + a_%d_%d*L_%d_%d + a_%d_%d*L_%d_%d"
+            " + L_%d_%d*L_%d_%d + b_%d_%d + L_%d_%d*L_%d_%d + L_%d_%d*L_%d_%d"         \
+            " + L_%d_%d*L_%d_%d + L_%d_%d*L_%d_%d + b_%d_%d*L_%d_%d + b_%d_%d*L_%d_%d" \
+            " + a_%d_%d*L_%d_%d + a_%d_%d*L_%d_%d"
 #define EQ3 "a_%d_%d + L_%d_%d*L_%d_%d"
 #define EQ4 "b_%d_%d + L_%d_%d*L_%d_%d"
 
 typedef struct
 {
-	char *eq;
+    char *eq;
 } polys;
 typedef struct
 {
-	polys *eqs;
-	uint64_t number_of_eqs;
+    polys *eqs;
+    uint64_t number_of_eqs;
 } output_of_polynomials;
 
 output_of_polynomials polynomials(uint32_t *plains, uint32_t *ciphers, int r, int number_of_plains);
