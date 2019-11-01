1f4c7c588ecf3ee6202f5cc7c1825a115156be1f
Hosein Hadipour <hsn.hadipour@gmail.com>
2019-11-01 16:20:48 +0330

    update speed.c

--- speed.c
+++ speed.c
@@ -7,9 +7,10 @@ double speed(int r)
     // Generateing 10000000 random plaintexts:
     uint64_t key = 0x5cec6701b79fd949;
     uint32_t plaintexts[NUMBER_OF_ENCRYPTIONS];
-    uint32_t *ciphertext;
-    time_t t;
-    ciphertext = (uint32_t *)malloc(NUMBER_OF_ENCRYPTIONS * sizeof(uint32_t));
+    //uint32_t *ciphertext;    
+    //ciphertext = (uint32_t *)malloc(NUMBER_OF_ENCRYPTIONS * sizeof(uint32_t));
+    uint32_t ciphertext = 0;
+    time_t t;    
     int i;
     clock_t start, end;
     double cpu_time_used;
@@ -23,8 +24,10 @@ double speed(int r)
     start = clock();
     for (i = 0; i < NUMBER_OF_ENCRYPTIONS; i++)
     {
-        keeloq_encrypt(&key, plaintexts + i, ciphertext + i, r);
+        //keeloq_encrypt(&key, plaintexts + i, ciphertext + i, r);
+        keeloq_encrypt(&key, plaintexts + i, &ciphertext, r);
     }
+    //free(ciphertext);
     end = clock();
     cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
     rate = (NUMBER_OF_ENCRYPTIONS * 4) / cpu_time_used;
